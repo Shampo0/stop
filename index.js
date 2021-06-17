5ad0aad05c3317ae536f5d6dc0551044c34069ff
Mije <im081377umyouo@gmail.com>
2021-06-17 19:53:46 +0900

    정선익 대머리

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 let express = require('express');
 let app = express();
 
-app.get('/', (req, res) => {
-    res.send(`Hello World!`);
-});
+app.set('view engine', 'ejs');
 
+app.use(express.json())
+
+app.use(express.urlencoded())
+app.get('/', (req, res) => {
+    res.render(`index`);
+});
+app.get('/test', (req, res) => {
+    res.render('test');
+});
+app.post('/submit', (req, res) => {
+    console.log(req.body);
+    res.redirect('/'); //경로로 리다이렉트
+})
 app.listen(3000, () => {
     console.log("서버 열림!");
 });
